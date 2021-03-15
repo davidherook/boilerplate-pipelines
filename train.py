bb70fa244f88eca0a0ba08545a92c05a6a1fb365
David Herook <david.herook@flywire.com>
2021-03-15 17:53:57 -0400

    Experimentation

--- train.py
+++ train.py
@@ -1,9 +1,14 @@
 import pickle
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.pipeline import Pipeline
+
+data_path = 'data/avocado_v1.csv'
 
 features = [
     'Total Volume', 
@@ -14,24 +19,71 @@ features = [
     'Small Bags', 
     'Large Bags',
     'XLarge Bags',
-    # 'type',
-    # 'year',
-    # 'region'
+    'type',
+    'year',
+    'region'
 ]
+
+numeric_features = ['Total Volume', 
+    '4046', 
+    '4225', 
+    '4770',
+    'Total Bags', 
+    'Small Bags', 
+    'Large Bags',
+    'XLarge Bags'
+]
+
+categorical_features = ['type',
+    'year',
+    'region']
 
 target = 'AveragePrice'
 
 if __name__ == '__main__':
 
-    df = pd.read_csv('data/avocado.csv')
+    df = pd.read_csv(data_path)
     print('Dataset: {}'.format(df.shape))
 
     X, y = df[features], df[target]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=7)
     print('Training on {}, testing on {}...'.format(len(X_train), len(X_test)))
 
+    # ==================================================================
+    # Show the difference in the datasets before, after transformation
+    # ==================================================================
+    print('\n' + '*'*40)
+    print('\nBefore Transformations:')
+    print('{}'.format(X_train.shape))
+    print(X_train)
+
+    numeric_transformer = Pipeline(steps = [
+        ('imputer', SimpleImputer(strategy='mean')),
+        ('scaler', StandardScaler())
+    ])
+
+    categorical_transformer = Pipeline(steps = [
+        ('imputer', SimpleImputer(strategy='constant')),
+        ('one hot', OneHotEncoder(handle_unknown='error'))
+    ])
+
+    transformer = ColumnTransformer(
+        [('numeric_transformer', numeric_transformer, numeric_features),
+         ('categorical_transformer', categorical_transformer, categorical_features)]
+    )
+
+    X_train_transf = transformer.fit_transform(X_train)
+
+    print('\nAfter Transformations:')
+    print('{}'.format(X_train_transf.shape))
+    print(pd.DataFrame(X_train_transf))
+    print('*'*40 + '\n')
+
+    # ==================================================================
+    # Create a master pipeline and fit the model
+    # ==================================================================
     pipe = Pipeline(steps=[
-        ('scaler', StandardScaler()),
+        ('transformer', transformer),
         ('regressor', LinearRegression())
     ])
 
@@ -40,6 +92,13 @@ if __name__ == '__main__':
 
     print('Train R2: {}'.format(model.score(X_train, y_train)))
     print('Test R2: {}'.format(model.score(X_test, y_test)))
+
+    # ==================================================================
+    # Show that fitting the transformed set returns the same as pipeline
+    # ==================================================================
+    model1 = LinearRegression().fit(X_train_transf, y_train)
+    print('Train R2: {}'.format(model1.score(X_train_transf, y_train)))
+    print('Test R2: {}'.format(model1.score(transformer.transform(X_test), y_test)))
 
 
 
