d0c4d37554ed5f35005fb82ae7fe236762095a1c
David Herook <david.herook@flywire.com>
2021-03-25 21:25:47 -0400

    Refactor

--- avocado_prices/predict.py
+++ avocado_prices/predict.py
@@ -5,8 +5,10 @@ from train import features, target
 df_path = 'data/avocado_test.csv'
 model_path = 'model/model.pk'
 
+
 if __name__ == '__main__':
 
+    print('\n' + '*'*40)
     df = pd.read_csv(df_path)
     df_out_path = df_path.replace('.csv', '_predictions.csv')
     print('Predicting {} samples...'.format(df.shape[0]))
@@ -16,3 +18,5 @@ if __name__ == '__main__':
     X_test = df[features]
     df[target + '_pred'] = model.predict(X_test)
     df.to_csv(df_out_path, index=False)
+    print(f'Predictions have been written to {df_out_path}')
+    print('*'*40 + '\n')
--- avocado_prices/train.py
+++ avocado_prices/train.py
@@ -8,7 +8,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.pipeline import Pipeline
 
-data_path = 'data/avocado_v1.csv'
+data_path = 'data/avocado.csv'
 
 features = [
     'Total Volume', 
@@ -36,14 +36,17 @@ numeric_features = ['Total Volume',
 
 categorical_features = ['type',
     'year',
-    'region']
+    'region'
+]
 
 target = 'AveragePrice'
 
+
 if __name__ == '__main__':
 
+    print('\n' + '*'*40)
     df = pd.read_csv(data_path)
-    print('Dataset: {}'.format(df.shape))
+    print('\nDataset: {}'.format(df.shape))
 
     X, y = df[features], df[target]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=7)
@@ -52,10 +55,7 @@ if __name__ == '__main__':
     # ==================================================================
     # Show the difference in the datasets before, after transformation
     # ==================================================================
-    print('\n' + '*'*40)
-    print('\nBefore Transformations:')
-    print('{}'.format(X_train.shape))
-    print(X_train)
+    print('\nBefore Transform: {}'.format(X_train.shape))
 
     numeric_transformer = Pipeline(steps = [
         ('imputer', SimpleImputer(strategy='mean')),
@@ -72,33 +72,24 @@ if __name__ == '__main__':
          ('categorical_transformer', categorical_transformer, categorical_features)]
     )
 
-    X_train_transf = transformer.fit_transform(X_train)
-
-    print('\nAfter Transformations:')
-    print('{}'.format(X_train_transf.shape))
-    print(pd.DataFrame(X_train_transf))
-    print('*'*40 + '\n')
+    print('After Transform: {}\n'.format(transformer.fit_transform(X_train).shape))
 
     # ==================================================================
     # Create a master pipeline and fit the model
     # ==================================================================
+    regressor = LinearRegression()
     pipe = Pipeline(steps=[
         ('transformer', transformer),
-        ('regressor', LinearRegression())
+        ('regressor', regressor)
     ])
 
     model = pipe.fit(X_train, y_train)
     pickle.dump(model, open('model/model.pk', 'wb'))
 
+
     print('Train R2: {}'.format(model.score(X_train, y_train)))
     print('Test R2: {}'.format(model.score(X_test, y_test)))
-
-    # ==================================================================
-    # Show that fitting the transformed set returns the same as pipeline
-    # ==================================================================
-    model1 = LinearRegression().fit(X_train_transf, y_train)
-    print('Train R2: {}'.format(model1.score(X_train_transf, y_train)))
-    print('Test R2: {}'.format(model1.score(transformer.transform(X_test), y_test)))
+    print('\n' + '*'*40 + '\n')
 
 
 
